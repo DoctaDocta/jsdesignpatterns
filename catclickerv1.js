bd822605e4a2ee8d7810e5def9763e701fef5669
DoctaDocta <trashm96@gmail.com>
2015-08-13 14:34:47 -0400

    update text is proper. Final Version

--- catclickerv1.js
+++ catclickerv1.js
@@ -1,26 +1,30 @@
 var count = 0;
 var cat_pic = $("#cat_pic");
 var catpic = $("#catpic");
-var scoreboard = $("#counter");
+var counter = $("#counter");
+var scoreboard = $("#scoreboard");
+var span = $("#span");
+
+//alert(span);
+
 
 if (scoreboard) {
 	console.log("scoreboard exists");
 }
 if (catpic){
 	console.log("catpic exists");
-	//catpic.click(scoreKeeper);
 	$(document).ready(function() {
-	$("#cat_pic").click(function() {
-		alert("your message");
-	    $("#scoreboard").text("Hello world!");
+		$("#cat_pic").on("click", function() {
+	   		count++;
+			$("#span").text(count);
+		});
 	});
-});
 }
 /*
 var scoreKeeper = function() {
 	count++;
 	console.log("catpic clicked to function");
-	scoreboard.html("<h3>You have clicked the pussy" + count + "times, now. Stop it.</h3>");
+	scoreboard.html("You have clicked the pussy" + count + "times, now. Stop it.");
 	return count;
 };*/
 
