d01f2057c5ea1cf1b9cd4bb8f5c8a2d17f0c4a8f
DoctaDocta <trashm96@gmail.com>
2015-08-21 18:45:18 -0400

    visual interface nearly complete.

--- catclickerv2.js
+++ catclickerv2.js
@@ -1,44 +1,53 @@
 <!-- View Model-->
 
-//class to represent
-
-function catBio(name) {
-	var self = this;
-	self.name = name;
-	self.score = 0;
-	self.pic = "/catpic.jpg";
-	console.log(self.pic);
+function randomCatPic(){
+	var dice = [1,2,3,4,5,6];
+	var diceRoll = sixSided[Math.floor(Math.random()*items.length)];
+	return diceRoll;
 };
 
+//the controller/octopus is acts between the model and the view.
+
+
+
+//the view is for what the user sees and interacts with
 function CatsViewModel() {
 	var self = this;
+	//initialize the main array display
+    self.cats = ko.observableArray([]);
+    self.numberOfCats = self.cats.length;
 
-	//initial cats. editable data
-    self.cats = ko.observableArray([
-    	new catBio('Bob'),
-    	new catBio('Becky'),
-    	new catBio('Barry'),
-    	new catBio('Blubb'),
-    	new catBio('Bertrand')
-    ]);
+	//initial cats.
+    // these should be in model cuz they're data.
+    self.initializeData = function() {
+    	var catNames = ['Bob', 'Becky', 'Barry','Blub','Bertrand'];
+    	var catNamesLength = catNames.length;
+        for (var i=0; i<catNamesLength;i++) {
+    		var cat = new catBio(catNames[i]);
+    		self.cats.push(cat);
+            console.log()
+    	};
+    };
+
+    //this should be in view
+    $(".table").on("load", self.initializeData());
+
+	//class to represent
+// this should be model.
+	function catBio(name) {
+		this.name = name;
+		this.score = "0";
+		//TO DO: implement randomCatPic on pic so there are diff pics
+		this.pic = "/cat-pictures/catpic.jpg";
+	};
 
     //Operations
-    self.appendNewCat = function() {
-    	self.cats.push(new catBio());
+    // this is the controller/octopus
+    self.appendNewCat = function(name) {
+    	var newKitten = new catBio(name);
+        console.log('new kitten: ' + newKitten.name + newKitten.score);
+    	self.cats.push(catobject);
     };
-
-    self.chosenCatId = ko.observable();
-    self.chosenCatData = ko.observable();
-
-    //behavior
-    self.goToCat = function(kitty) {
-    	self.chosenCatId(kitty);
-    	$.get('/catpic.jpg', {cats: kitty}, self.chosenCatData);
-
-    };
-
-
-
 };
 
 $(document).ready(function() {
